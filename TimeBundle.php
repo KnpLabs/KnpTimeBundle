0b4e2d07bc2acc3362e1118682af207ee77342aa
Antoine Hérault <antoine.herault@gmail.com>
2011-01-22 00:25:45 +0100

    Implement two new bundle interface methods

--- TimeBundle.php
+++ TimeBundle.php
@@ -6,4 +6,13 @@ use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
 
 class TimeBundle extends BaseBundle
 {
+    public function getNamespace()
+    {
+        return __NAMESPACE__;
+    }
+
+    public function getPath()
+    {
+        return __DIR__;
+    }
 }
