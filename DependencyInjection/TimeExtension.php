71df0f2af41d52d3210bca86d1a53e7085d916a2
ornicar <thibault.duplessis@gmail.com>
2011-02-13 18:21:28 -0800

    Update namespace in DIC extension

--- DependencyInjection/TimeExtension.php
+++ DependencyInjection/TimeExtension.php
@@ -5,7 +5,7 @@ namespace Knplabs\TimeBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\FileLocator;
+use Symfony\Component\Config\FileLocator;
 
 class TimeExtension extends Extension
 {
