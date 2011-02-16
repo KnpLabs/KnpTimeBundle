6025ed84b529155047f4122d1d8d5e2161a096e4
Florian Klein <florian.klein@knplabs.com>
2011-02-16 10:20:29 +0100

    renaming class name for autoload

--- DependencyInjection/KnplabsTimeExtension.php
+++ DependencyInjection/KnplabsTimeExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 
-class TimeExtension extends Extension
+class KnplabsTimeExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
