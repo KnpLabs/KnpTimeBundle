3062c4f9c43ee877a36b452a4ca61fda3379ca0f
ornicar <thibault.duplessis@gmail.com>
2011-03-06 11:31:23 -0800

    Modernize bundle and DIC extension classes

--- DependencyInjection/KnplabsTimeExtension.php
+++ DependencyInjection/KnplabsTimeExtension.php
@@ -2,7 +2,7 @@
 
 namespace Knplabs\TimeBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -11,37 +11,8 @@ class KnplabsTimeExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        foreach (self::normalizeKeys($configs) as $config) {
-            $this->doConfigLoad($config, $container);
-        }
-    }
-
-    public function doConfigLoad(array $config, ContainerBuilder $container)
-    {
-        if(!$container->hasDefinition('time.templating.helper.time')) {
-            $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-            $loader->load('templating.xml');
-            $loader->load('twig.xml');
-        }
-    }
-
-    /**
-     * Returns the base path for the XSD files.
-     *
-     * @return string The XSD base path
-     */
-    public function getXsdValidationBasePath()
-    {
-        return null;
-    }
-
-    public function getNamespace()
-    {
-        return 'http://www.symfony-project.org/schema/dic/symfony';
-    }
-
-    public function getAlias()
-    {
-        return 'knplabs_time';
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('templating.xml');
+        $loader->load('twig.xml');
     }
 }
--- KnplabsTimeBundle.php
+++ KnplabsTimeBundle.php
@@ -6,13 +6,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
 
 class KnplabsTimeBundle extends BaseBundle
 {
-    public function getNamespace()
-    {
-        return __NAMESPACE__;
-    }
-
-    public function getPath()
-    {
-        return strtr(__DIR__, '\\', '/');
-    }
 }
