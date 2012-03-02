5801921732f4639f68687c22df2db73130074479
Dmitry [DNK] <artseld@gmail.com>
2012-03-02 10:37:41 +0300

    rework age function

--- DateTimeFormatter.php
+++ DateTimeFormatter.php
@@ -9,31 +9,6 @@ class DateTimeFormatter
 {
     protected $translator;
 
-    // Set age locales
-    // FORMAT: locale => group key
-    protected $ageConfigLocales = array(
-        'ru'    => 'ru_group',  // Russia
-        'be'    => 'ru_group',  // Belarus
-        // add another locales if need
-    );
-
-    // Set age configuration
-    // FORMAT: group key => [ template => translator key for string suffix ]
-    protected $ageConfigGroups = array(
-        // default group, mandatory
-        'default' => array(
-            '~' => 'years', // default value, mandatory
-            '1' => 'year',  // template (value or ~N construction, e.g. `~13` matches 7613, 13, etc.) | excluded numbers
-        ),
-        // custom group
-        'ru_group' => array(
-            '~'                 => 'years',
-            '~1|11'             => 'year',
-            '~2,~3,~4|12,13,14' => 'year_',
-        ),
-        // add another groups if need
-    );
-
     /**
      * Constructor
      *
@@ -127,72 +102,19 @@ class DateTimeFormatter
      */
     public function formatAge(Datetime $birthdate, Datetime $current)
     {
-        $locale = $this->translator->getLocale();
-        $config = $this->ageConfigGroups['default'];
-
-        if (isset($this->ageConfigLocales[$locale]) &&
-            isset($this->ageConfigGroups[$this->ageConfigLocales[$locale]])) {
-            $config = $this->ageConfigGroups[$this->ageConfigLocales[$locale]];
-        }
-
         $yearsDiff = $birthdate->diff($current);
 
         if ($yearsDiff->invert) {
-            return $this->getIncorrectAgeMessage();
+            return $this->translator->trans('age.incorrect', array(), 'time');
         }
 
-        // prepare variables
         $y = $yearsDiff->y;
-        $yearsMinimized = $y;
 
-        if ($yearsMinimized > 100) {
-            $yearsMinimized = $yearsMinimized%100;
+        if ($y == 0) {
+            return $this->translator->trans('age.null', array(), 'time');
         }
 
-        $yearsSuffix = $config['~'];
-        $yearsMinimizedLength = strlen($yearsMinimized);
-
-        // get years suffix
-        foreach ($config as $k => $v) {
-            if ($k == '~') continue;
-            $rules = explode('|', $k);
-            if (isset($rules[1])) {
-                $excluded = explode(',', $rules[1]);
-                if (in_array($yearsMinimized, $excluded)) continue;
-            }
-            $used = explode(',', $rules[0]);
-            if (in_array($yearsMinimized, $used)) {
-                $yearsSuffix = $v;
-                break;
-            }
-            foreach ($used as $tpl) {
-                $tpl = ltrim($tpl, '~');
-                if (strrpos($yearsMinimized, $tpl) === $yearsMinimizedLength - strlen($tpl)) {
-                    $yearsSuffix = $v;
-                    break;
-                }
-            }
-        }
-
-        return $this->getAgeMessage($y, $yearsSuffix);
+        return $this->translator->transChoice('age.years', $y, array('%count%' => $y), 'time');
     }
 
-    /**
-     * @param $years
-     * @param $key
-     *
-     * @return string
-     */
-    protected function getAgeMessage($years, $key)
-    {
-        return $years . ' ' . $this->translator->trans('knp_time.text.age_suffix.' . $key);
-    }
-
-    /**
-     * @return string
-     */
-    public function getIncorrectAgeMessage()
-    {
-        return $this->translator->trans('knp_time.text.age_incorrect');
-    }
 }
