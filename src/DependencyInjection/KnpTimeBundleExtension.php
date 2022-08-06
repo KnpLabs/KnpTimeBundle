3f349773895e2a275ff09e11d6807c5e4e94a808
Jesse Rushlow <jr@rushlow.dev>
2022-08-06 17:02:10 -0400

    fix extension path

--- src/DependencyInjection/KnpTimeBundleExtension.php
+++ src/DependencyInjection/KnpTimeBundleExtension.php
@@ -13,7 +13,7 @@ class KnpTimeBundleExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(\dirname(__DIR__).'/Resources/config'));
         $loader->load('services.xml');
 
         if (class_exists(AbstractExtension::class)) {
